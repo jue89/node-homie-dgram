5a2d7fe3c94da8eec779c934230cf61414830d52
Jue <me@jue.yt>
2021-04-11 10:36:49 +0100

    add factory

---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = async ({port, mcast} = {}) => {
+	const {PktIn, PktOutInfoRequest, PktOutStateSet} = require('./lib/pkt.js');
+
+	function parsePkt (payload) {
+		return PktIn.fromBuffer(payload);
+	}
+
+	const {Socket} = require('./lib/net.js')({parsePkt});
+
+	const socket = new Socket();
+
+	function setState (cpuid, id, val) {
+		const pkt = new PktOutStateSet(cpuid);
+		pkt.set(id, val);
+		socket.send(pkt);
+	}
+
+	function requestInfo (cpuid) {
+		const pkt = new PktOutInfoRequest(cpuid);
+		socket.send(pkt);
+	}
+
+	const {DeviceStore} = require('./lib/devices.js')({setState, requestInfo});
+
+	const devices = new DeviceStore();
+
+	socket.on('pkt', (pkt) => devices.handlePkt(pkt));
+
+	await socket.bind(port, mcast);
+
+	return {
+		get: devices.get.bind(devices),
+		getAll: devices.getAll.bind(devices)
+	};
+};
